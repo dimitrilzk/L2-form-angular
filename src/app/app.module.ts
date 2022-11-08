1dc80da25e6238b7f01cd239f1dac044a1549aae
dimitrilzk <dimitrilazuka95@gmail.com>
2022-11-08 18:23:39 +0100

    starte pack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Form1Component } from './form1/form1.component';
+import { Form2Component } from './form2/form2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Form1Component,
+    Form2Component
   ],
   imports: [
     BrowserModule,
